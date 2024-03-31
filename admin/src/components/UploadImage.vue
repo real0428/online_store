2a981726f320ec8ef82a674256d7cdc3f152d026
real0428 <real0428@gmail.com>
2024-03-31 10:55:43 +0800

    新增圖片上傳組件

---
+++ admin/src/components/UploadImage.vue
@@ -0,0 +1,87 @@
+<template>
+  <el-upload class="avatar-uploader" :auto-upload="false" :on-change="getFile" action="#" :show-file-list="false"
+    :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+    <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+    <el-icon v-else class="avatar-uploader-icon">
+      <Plus />
+    </el-icon>
+  </el-upload>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import { ElMessage } from 'element-plus'
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadProps } from 'element-plus'
+
+const emit = defineEmits(['get-file'])
+
+const props = defineProps({
+  image: {
+    type: String,
+    default: ''
+  }
+})
+
+const imageUrl = ref('')
+
+watch(() => props.image, (image) => {
+  imageUrl.value = image
+})
+
+const getFile = (file: any) => {
+  const objectURL = URL.createObjectURL(file.raw)
+  imageUrl.value = objectURL
+  emit('get-file', file.raw)
+}
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (
+  response,
+  uploadFile
+) => {
+  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
+}
+
+/* const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+  console.log(123);
+  if (rawFile.type !== 'image/jpeg') {
+    ElMessage.error('檔案須為jpg格式!')
+    return false
+  } else if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('檔案不能超過2MB!')
+    return false
+  }
+  return true
+} */
+</script>
+
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
