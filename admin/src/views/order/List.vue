4cf24fc4b524db10e335316b76efe413bb98ae5d
real0428 <real0428@gmail.com>
2024-04-19 18:44:24 +0800

    新增後台的訂單頁

--- admin/src/views/order/List.vue
+++ admin/src/views/order/List.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    訂單列表
-  </div>
+  <h1 class="mb-6">訂單列表</h1>
+  <!-- <el-table :data="news" style="width: 100%">
+    <el-table-column prop="item_id" label="ID" width="100" />
+    <el-table-column prop="img_url" label="縮圖" width="180">
+      <template #default="scope">
+        <el-image class="!flex" style="width: 100px;height:50px" :src="scope.row.img_url" fit="cover" />
+      </template>
+</el-table-column>
+<el-table-column prop="title" label="標題" />
+<el-table-column label="操作" width="180">
+  <template #default="scope">
+        <el-button type="primary" @click="handleEdit(scope.row.item_id)">編緝</el-button>
+        <el-button type="danger" @click="handleDelete(scope.row)">刪除</el-button>
+      </template>
+</el-table-column>
+</el-table> -->
 </template>
