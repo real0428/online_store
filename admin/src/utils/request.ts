a5f3556cbda702c28c829a35ac173c48616425fb
real0428 <real0428@gmail.com>
2024-04-19 18:30:00 +0800

    新增後台管理的api請求方法

--- admin/src/utils/request.ts
+++ admin/src/utils/request.ts
@@ -1,26 +1,30 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
+import { useAuthStore } from '@/stores/user'
 
 let request = axios.create({
-  baseURL: '',
+  baseURL: 'http://127.0.0.1/api',
   timeout: 5000
 })
 
 request.interceptors.request.use(config => {
-  config.headers.Authorization = ''
+  config.headers.Authorization = JSON.parse(localStorage.getItem('user') as string)?.token || ''
   return config
 })
 
 request.interceptors.response.use(response => {
   return response.data
 }, error => {
-  const message = error.message;
   console.log(error);
-
+  const message = error.response.data.message;
   ElMessage({
     type: 'error',
     message,
   })
+  const { clearToken } = useAuthStore()
+  setTimeout(() => {
+    clearToken()
+  }, 1000)
   return Promise.reject(error)
 })
 
