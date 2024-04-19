62b8955c690e99df25414e06e51525b44e3a3a04
real0428 <real0428@gmail.com>
2024-04-19 18:11:17 +0800

    新增會員相關api

--- admin/src/api/user/user.ts
+++ admin/src/api/user/user.ts
@@ -1,4 +1,30 @@
 import request from '@/utils/request'
-console.log(request);
-
 export const getUsers = () => request.get('https://randomuser.me/api/');
+
+export const getAccessAuth = (form: any) => request({
+  method: 'post',
+  url: '/business_user/login',
+  data: form
+})
+
+export const getUserInfo = () => request({
+  url: '/business_user/info'
+})
+
+export const updateUserInfo = (form: any) => {
+  const formData = new FormData()
+  console.log(form.image);
+  formData.append('username', form.username)
+  if (form.image) {
+    formData.append('image', form.image)
+  }
+  formData.append('email', form.email)
+  formData.append('nickname', form.nickname)
+
+  return request({
+    method: 'put',
+    url: '/business_user/info',
+    data: formData
+  })
+
+} 
--- server/router/business_user.js
+++ server/router/business_user.js
@@ -4,6 +4,8 @@ const bycript = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authMiddleWare = require('../middleware/auth')
 const db = require('../db')
+const multer = require('multer')
+const upload = multer({ dest: __dirname + '../../uploads' })
 require('dotenv').config()
 
 // 賣家用戶註冊
@@ -34,13 +36,13 @@ router.post('/business_user/login', (req, res) => {
     if (err) return res.cc(err)
     if (results.length === 0) return res.status(422).cc('用戶不存在')
     const compareResult = bycript.compareSync(password, results[0].password)
-    if (!compareResult) return res.cc('密碼錯誤')
+    if (!compareResult) return res.status(401).cc('密碼錯誤')
     const user = {
       ...results[0],
       password: '',
       user_pic: ''
     }
-    const token = jwt.sign(user, process.env.JWTSECRETKEY, { expiresIn: '10h' })
+    const token = jwt.sign(user, process.env.JWTSECRETKEY, { expiresIn: '2h' })
     res.send({
       status: 0,
       message: '登入成功',
@@ -67,14 +69,35 @@ router.get('/business_user/info', authMiddleWare, (req, res) => {
 })
 
 //更新賣家用戶資料
-router.put('/business_user/info', authMiddleWare, (req, res) => {
+router.put('/business_user/info', authMiddleWare, upload.single('image'), (req, res) => {
   const { id } = req.auth
-  const { nickname, email, user_pic } = req.body
+  const file = req.file
+  console.log(req.body);
+  let info = {}
+  if (file) {
+    file.url = `http://127.0.0.1/uploads/${file.filename}`
+    info = {
+      ...req.body,
+      user_pic: file.url
+    }
+  } else {
+    info = {
+      ...req.body
+    }
+  }
   const sql = `UPDATE business_users SET ? WHERE id=?`
-  db.query(sql, [{ nickname, email, user_pic }, id], (err, results) => {
+  db.query(sql, [info, id], (err, results) => {
     if (err) return res.cc(err)
-    if (results.affectedRows !== 1) return res.cc('更新失敗')
-    res.cc(0, '更新成功')
+    if (results.affectedRows !== 1) return res.status(400).cc('更新失敗')
+    const sql = `SELECT username, user_pic, nickname FROM business_users WHERE id=?`
+    db.query(sql, id, (err, results) => {
+      if (err) return res.cc(err)
+      res.send({
+        status: 0,
+        message: '更新成功',
+        data: results[0]
+      })
+    })
   })
 })
 
